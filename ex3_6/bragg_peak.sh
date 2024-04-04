bdd17b83311459468537a38e019c68b159165848
Brian Rogers <br@Brians-MacBook-Pro.local>
2024-04-04 16:13:02 +0100

    ex 3.6

--- ex3_6/bragg_peak.sh
+++ ex3_6/bragg_peak.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 export TOPAS_G4_DATA_DIR="/Applications/G4Data"
 export QT_QPA_PLATFORM_PLUGIN_PATH="/Applications/topas/Frameworks"
-thickness=5
-t=$(echo "scale=2; $thickness / 100" | bc)
-"./part_b/outputs_pos$pos/job$i"
-while [ $thickness -lt 35 ]
-do
-mkdir ./part_a ./part_a/jobs_thick$thickness ./part_a/outputs_thick$thickness
+mkdir ./bragg_peak ./bragg_peak/jobs ./bragg_peak/outputs
 i=1
-
-while [ $i -lt 4 ]
+``
+while [ $i -lt 11 ]
 do
-cat <<EOF > ./part_a/jobs_thick$thickness/job$i.txt
+cat <<EOF > ./bragg_peak/jobs/job$i.txt
 includeFile = ./base.txt 
 i:Ts/Seed = $i
 
@@ -19,7 +14,7 @@ i:Ts/Seed = $i
 s:Ge/Plate/Type="TsCylinder"
 s:Ge/Plate/Parent="Phantom"
 s:Ge/Plate/Material="Lead"
-d:Ge/Plate/HL = $t cm
+d:Ge/Plate/HL = 0.23 cm
 d:Ge/Plate/RMin = 0. cm
 d:Ge/Plate/RMax = 15. cm
 d:Ge/Plate/SPhi = 0. deg
@@ -33,11 +28,8 @@ s:Sc/DoseScorer/Component = "Phantom" # Where to score
 b:Sc/DoseScorer/OutputToConsole = "TRUE" # Output
 s:Sc/DoseScorer/IfOutputFileAlreadyExists = "Overwrite"
 i:Sc/DoseScorer/ZBins = 1000
-s:Sc/DoseScorer/OutputFile = "./part_a/outputs_thick$thickness/job$i"
+s:Sc/DoseScorer/OutputFile = "./bragg_peak/outputs/job$i"
 EOF
-/Applications/topas/bin/topas ./part_a/jobs_thick$thickness/job$i.txt
+/Applications/topas/bin/topas ./bragg_peak/jobs/job$i.txt
 ((i ++))
-done
-((thickness+=5))
-t=$(echo "scale=2; $thickness / 100" | bc)
 done
